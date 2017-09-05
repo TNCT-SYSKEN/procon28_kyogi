c4cc3f12305449efdfbde8cefc7afb6187838f7e
amataku <amataku13@gmail.com>
2017-09-05 23:35:23 +0900

    fix color change

--- pazzfit/Control.cpp
+++ pazzfit/Control.cpp
@@ -18,10 +18,10 @@ void Control::output_piece() {
 		for (int i = 0; i < piece.size(); i++) {
 				for (int j = 0; j < piece[i].ans_point.size(); j++) {
 						if (j != piece[i].ans_point.size()) {
-								Line(piece[i].ans_point[j].first * 5, piece[i].ans_point[j].second * 5, piece[i].ans_point[j + 1].first * 5, piece[i].ans_point[j + 1].second * 5).draw(Color(i + 4, i + 4, i + 4));
+								Line(piece[i].ans_point[j].first * 5, piece[i].ans_point[j].second * 5, piece[i].ans_point[j + 1].first * 5, piece[i].ans_point[j + 1].second * 5).draw(Color(i + 1, i + 2, i + 3));
 						}
 						else {
-								Line(piece[i].ans_point[j].first * 5, piece[i].ans_point[j].second * 5, piece[i].ans_point[0].first * 5, piece[i].ans_point[0].second * 5).draw(Color(i + 4, i + 4, i + 4));
+								Line(piece[i].ans_point[j].first * 5, piece[i].ans_point[j].second * 5, piece[i].ans_point[0].first * 5, piece[i].ans_point[0].second * 5).draw(Color(i + 1, i + 2, i + 3));
 						}
 				}
 		}
